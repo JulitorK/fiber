b50736d9107bba5b075757d8ccb7a87f0257763c
JulitorK <jkr@sunday.dk>
2019-09-26 16:14:51 +0300

    added jsx support

---
+++ dist/app.js
@@ -0,0 +1,81 @@
+"use strict";
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+/** @jsx createElement */
+window.onload = function () {
+  var root = document.getElementById("root");
+  var TEXT_ELEMENT = "TEXT ELEMENT";
+  var element = createElement("div", null, createElement("div", null, "hai"), createElement("div", null, "it works"), createElement("input", {
+    type: "text",
+    value: "john"
+  }));
+
+  function render(node, parentNode) {
+    var type = node.type,
+        props = node.props;
+    var isTextElement = type === TEXT_ELEMENT;
+    var dom = isTextElement ? document.createTextNode("") : document.createElement(type);
+
+    var isListener = function isListener(name) {
+      return name.startsWith("on");
+    };
+
+    Object.keys(props).filter(isListener).forEach(function (name) {
+      var eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, props[name]);
+    });
+
+    var isAttribute = function isAttribute(name) {
+      return !isListener(name) && name !== "children";
+    };
+
+    Object.keys(props).filter(isAttribute).forEach(function (name) {
+      dom[name] = props[name];
+    });
+    var childElements = props.children || [];
+    childElements.forEach(function (childElement) {
+      return render(childElement, dom);
+    });
+    parentNode.append(dom);
+  }
+
+  function createElement(type, config) {
+    console.log('works');
+
+    var props = _objectSpread({}, config);
+
+    for (var _len = arguments.length, args = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+      args[_key - 2] = arguments[_key];
+    }
+
+    var hasChildren = args.length > 0;
+    var rawChildren = hasChildren ? [].concat(args) : [];
+    props.children = rawChildren.filter(function (e) {
+      return e !== null && e !== undefined;
+    }).map(function (e) {
+      return e instanceof Object ? e : createTextElement(e);
+    });
+    console.log('{type, props}', {
+      type: type,
+      props: props
+    });
+    return {
+      type: type,
+      props: props
+    };
+  }
+
+  function createTextElement(value) {
+    return createElement(TEXT_ELEMENT, {
+      nodeValue: value
+    });
+  }
+
+  render(element, root);
+};
+//# sourceMappingURL=app.js.map
--- src/app.js
+++ src/app.js
@@ -1,41 +1,61 @@
+/** @jsx createElement */
 window.onload = () => {
-    const root = document.getElementById('root');
-    const element = {
-        type: 'div',
-        props: {
-            id: 'container',
-            children: [
-                { type: 'input', props: {value: 'foo', type: 'text'} },
-                { type: 'a', props: {href: '/bar'} },
-                { type: 'span', props: { children: [{type: 'TEXT ELEMENT', props: { nodeValue: 'Bar'}}]} }
-            ]
-        }
-    };
+  const root = document.getElementById("root");
+  const TEXT_ELEMENT = "TEXT ELEMENT";
+  const element = (
+    <div>
+      <div>hai</div>
+      <div>it works</div>
+      <input type="text" value="john"/>
+    </div>
+  );
 
-    function render(node, parentNode){
-        const {type, props} = node;
+  function render(node, parentNode) {
+    const { type, props } = node;
 
-        const isTextElement = type === 'TEXT ELEMENT';
+    const isTextElement = type === TEXT_ELEMENT;
 
-        const dom = isTextElement ? document.createTextNode("") : document.createElement(type);
+    const dom = isTextElement
+      ? document.createTextNode("")
+      : document.createElement(type);
 
-        const isListener = (name) => name.startsWith('on');
+    const isListener = name => name.startsWith("on");
 
-        Object.keys(props).filter(isListener).forEach(name => {
-            const eventType = name.toLowerCase().substring(2);
-            dom.addEventListener(eventType, props[name]);
-        });
+    Object.keys(props)
+      .filter(isListener)
+      .forEach(name => {
+        const eventType = name.toLowerCase().substring(2);
+        dom.addEventListener(eventType, props[name]);
+      });
 
-        const isAttribute = (name) => !isListener(name) && name !== 'children';
+    const isAttribute = name => !isListener(name) && name !== "children";
 
-        Object.keys(props).filter(isAttribute).forEach(name => {
-           dom[name] = props[name];
-        });
-        const childElements = props.children || [];
+    Object.keys(props)
+      .filter(isAttribute)
+      .forEach(name => {
+        dom[name] = props[name];
+      });
+    const childElements = props.children || [];
 
-        childElements.forEach(childElement => render(childElement, dom));
-        parentNode.append(dom);
-    }
+    childElements.forEach(childElement => render(childElement, dom));
+    parentNode.append(dom);
+  }
 
-    render(element, root);
+  function createElement(type, config, ...args){
+    console.log('works');
+    const props = {...config};
+    const hasChildren = args.length > 0;
+    const rawChildren = hasChildren ? [...args] : [];
+    props.children = rawChildren
+      .filter(e => e !== null && e !== undefined)
+      .map(e => e instanceof Object ? e : createTextElement(e));
+    console.log('{type, props}', {type, props});
+    return {type, props}
+  }
+
+  function createTextElement(value){
+    return createElement(TEXT_ELEMENT, {nodeValue: value})
+  }
+
+  render(element, root);
 };
